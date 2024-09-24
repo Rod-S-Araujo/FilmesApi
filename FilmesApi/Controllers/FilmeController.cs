6a90aa0a46cbee816192ac85a809661498f5033d
Rodrigo <rods_araujo@hotmail.com>
2024-09-24 09:54:25 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,90 @@
+﻿using AutoMapper;
+using FilmesApi.Data;
+using FilmesApi.Data.Dtos;
+using FilmesApi.Models;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private FilmeContext _context;
+    private IMapper _mapper;
+
+    public FilmeController(FilmeContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public IActionResult AdicionarFilme([FromBody]CreateFilmeDto filmeDto)
+    {
+        Filme filme = _mapper.Map<Filme>(filmeDto);
+        _context.Filmes.Add(filme);
+        _context.SaveChanges();
+        return CreatedAtAction(nameof(RecuperarFilmePorId), new { id = filme.Id }, filme);
+    }
+
+    [HttpGet]
+    public IEnumerable<ReadFilmeDto> RecuperaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50)
+    {
+        return _mapper.Map<List<ReadFilmeDto>>(_context.Filmes.Skip(skip).Take(take));
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult RecuperarFilmePorId(int id) 
+    {
+        var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null) return NotFound();
+        var filmeDto = _mapper.Map<ReadFilmeDto>(filme);
+        return Ok(filmeDto);
+
+    }
+
+    //Metodo put de atualizar Filme, onde ele precisa dos parametros id e do UpdateFilmeDto que ele vai extrair do corpo da requisição, no primeiro momento ele busca no banco de dados um filme que corresponda ao Id do filme a alterar, caso não encontre o Id a alterar ele retorna um notfound, caso encontre ele utiliza o mapper para pegar os dados do DTO para padrão filme e salva as ações no Db.
+    [HttpPut("{id}")]
+    public IActionResult AtualizaFilme(int id, [FromBody] UpdateFilmeDto filmeDto )
+    {
+        var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null) return NotFound();
+        _mapper.Map(filmeDto, filme);
+        _context.SaveChanges();
+        return NoContent();
+    }
+
+    //Metodo patch de atualizar parcialmente o Filme, utiliza o NewtonSoft(JsonPatchDocument) para destruir os dados do UpdateFimeDto, em seguida ele recupera o filmeparaAtualizar convertendo para o padrão UpdateFilmeDto o filme recuperado se encontrado, se ApplyTo tiver um ModelState valido  ele salva as alterações se não retorna um ValidationProblem.
+    [HttpPatch("{id}")]
+    public IActionResult AtualizaFilmeParcial(int id, JsonPatchDocument<UpdateFilmeDto> patch)
+    {
+        var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null) return NotFound();
+
+        var filmeParaAtualizar = _mapper.Map<UpdateFilmeDto>(filme);
+
+        patch.ApplyTo(filmeParaAtualizar, ModelState);
+        if(!TryValidateModel(filmeParaAtualizar))
+        {
+            return ValidationProblem(ModelState);
+        }
+        _mapper.Map(filmeParaAtualizar, filme);
+        _context.SaveChanges();
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeletaFilme(int id)
+    {
+        var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null) return NotFound();
+        _context.Remove(filme);
+        _context.SaveChanges();
+        return NoContent();
+    }
+}
+
+
---
+++ FilmesApi/Migrations/20240920134436_CorrigindoTabela.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FilmesApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class CorrigindoTabela : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Filters",
+                table: "Filters");
+
+            migrationBuilder.RenameTable(
+                name: "Filters",
+                newName: "Filmes");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Filmes",
+                table: "Filmes",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Filmes",
+                table: "Filmes");
+
+            migrationBuilder.RenameTable(
+                name: "Filmes",
+                newName: "Filters");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Filters",
+                table: "Filters",
+                column: "Id");
+        }
+    }
+}
