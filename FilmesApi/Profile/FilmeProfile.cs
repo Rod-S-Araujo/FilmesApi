7dc4acb3b1bc1e51d963b3087df1eec378550fcb
Rodrigo <rods_araujo@hotmail.com>
2024-09-26 00:44:57 -0300

    Atualizando FilmesApi

--- FilmesApi/Profile/FilmeProfile.cs
+++ FilmesApi/Profile/FilmeProfile.cs
@@ -11,6 +11,6 @@ public class FilmeProfile : Profile
         CreateMap<CreateFilmeDto, Filme>();
         CreateMap<UpdateFilmeDto, Filme>();
         CreateMap<Filme, UpdateFilmeDto>();
-        CreateMap<Filme, ReadFilmeDto>();
+        CreateMap<Filme, ReadFilmeDto>().ForMember(FilmeDto => FilmeDto.Sessoes, opt => opt.MapFrom(filme => filme.Sessoes));
     }
 }
